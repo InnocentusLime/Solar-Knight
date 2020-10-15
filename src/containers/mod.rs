612ddab0505ab54462ed0f8ae13dfdde9bdc9766
InnocentusLime <InnocentusLime.help@gmail.com>
2020-10-15 18:30:50 +0300

    Removed custom containers til they are needed

--- src/containers/mod.rs
+++ src/containers/mod.rs
@@ -1,9 +1,9 @@
-pub mod linked_lists;
-pub mod consistent_linear_chunk;
-pub mod consistent_chunk;
+//pub mod linked_lists;
+//pub mod consistent_linear_chunk;
+//pub mod consistent_chunk;
 pub mod memory_chunk;
 
-pub use linked_lists::LinkedLists;
-pub use consistent_linear_chunk::ConsistentLinearChunk;
-pub use consistent_chunk::ConsistentChunk;
+//pub use linked_lists::LinkedLists;
+//pub use consistent_linear_chunk::ConsistentLinearChunk;
+//pub use consistent_chunk::ConsistentChunk;
 pub use memory_chunk::MemoryChunk;
